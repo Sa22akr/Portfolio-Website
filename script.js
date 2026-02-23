c2b2a9c2f3d7ec4743c2e99c2c09ab016a56c20b
Sa22akr <sa22akr@herts.ac.uk>
2026-02-23 22:50:22 +0000

    - Added GitHub and Live Demo buttons to projects - Refined hero section spacing and layout - Fixed background layering issue - Improved card spacing and responsiveness

--- script.js
+++ script.js
@@ -44,3 +44,14 @@ const year = document.getElementById("year");
 if (year) {
   year.textContent = new Date().getFullYear();
 }
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showNextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(showNextSlide, 3000); // change every 3 seconds
